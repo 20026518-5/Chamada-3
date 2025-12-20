4e5ab57b81a3c3ec866f71feab76afb32193a214
Lucas <lucasviniciussampaiolima@gmail.com>
2025-12-20 11:58:02 -0400

    Primeiro commit

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -1,21 +1,20 @@
 import { initializeApp } from 'firebase/app'
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage';
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA0x_v_CsyIftRBhqKEOmaEGRkXRpdqJdU",
-  authDomain: "sistema-chamados-69279.firebaseapp.com",
-  projectId: "sistema-chamados-69279",
-  storageBucket: "sistema-chamados-69279.appspot.com",
-  messagingSenderId: "111999225555",
-  appId: "1:111999225555:web:2b7805d2e64a3d7d560e26",
-  measurementId: "G-TZ0MBXYFYS"
+  apiKey: "AIzaSyCKsDUmGU2B7p7fFu_-8Tpk6YACJDiD060",
+  authDomain: "teste-chamada-3a923.firebaseapp.com",
+  projectId: "teste-chamada-3a923",
+  storageBucket: "teste-chamada-3a923.firebasestorage.app",
+  messagingSenderId: "357349991534",
+  appId: "1:357349991534:web:23aad0a159db2231daf27f",
+  measurementId: "G-2DP1JVXZ9K"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
-
 const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
 const storage = getStorage(firebaseApp);
