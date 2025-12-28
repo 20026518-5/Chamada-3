69cc9b541b90c242495da6da58f767617d236827
Lucas Vinicius / INTELLEART <147888270+20026518-5@users.noreply.github.com>
2025-12-28 15:14:13 -0400

    Use Loading component during authentication loading
    Replace loading message with Loading component for better UX.

--- src/routes/private.js
+++ src/routes/private.js
@@ -1,10 +1,15 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../contexts/auth";
+import Loading from "../components/Loading";
 
 export default function Private ({children}){
   const {signed,loading} = useContext(AuthContext);
-  if(loading) return <h1>Carregando...</h1>
+  if(loading) return  (
+  <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+    <Loading size={40} color="#121212" />
+  </div>
+);
   if(!signed) return <Navigate to='/' />
   
   //console.log(signed);
