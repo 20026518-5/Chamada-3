e2fcbaf3dad419e9257612e6bfe65a5d4832a63f
Lucas Vinicius / INTELLEART <147888270+20026518-5@users.noreply.github.com>
2025-12-31 10:23:21 -0400

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,34 +1,40 @@
 import { useContext } from 'react';
 import avatarImg from '../../assets/avatar.png';   
 import { AuthContext } from '../../contexts/auth';
-import {FiHome, FiSettings, FiUser} from 'react-icons/fi';
+// 1. Adicione o FiLogOut nos imports
+import {FiHome, FiSettings, FiUser, FiLogOut} from 'react-icons/fi'; 
 import { Link } from 'react-router-dom';
 import './header.css';
 
 export default function Header() {
-  const {user} = useContext(AuthContext);
-return (
-  <div className='sidebar'>
-    <div>
-      <img src={user.avatarUrl === null ? avatarImg : user.avatarUrl} alt = 'foto do usuario' />
+  // 2. Pegue a função logOut do seu contexto
+  const {user, logOut} = useContext(AuthContext); 
+
+  return (
+    <div className='sidebar'>
+      <div>
+        <img src={user.avatarUrl === null ? avatarImg : user.avatarUrl} alt = 'foto do usuario' />
+      </div>
+      <Link to='/dashboard'>
+        <FiHome color='#fff' size={24} />
+        Chamados
+      </Link>
+
+      <Link to='/customers'>
+        <FiUser color='#fff' size={24} />
+        Clientes
+      </Link>
+
+      <Link to='/profile'>
+        <FiSettings color='#fff' size={24} />
+        Perfil
+      </Link>
+
+      {/* 3. Adicione o botão de logout */}
+      <button onClick={ () => logOut() } className="logout-btn">
+        <FiLogOut color='#fff' size={24} />
+        Sair
+      </button>
     </div>
-    <Link to='/dashboard'>
-    <FiHome color='#fff' size={24} />
-    Chamados
-    </Link>
-
-    <Link to='/customers'>
-    <FiUser color='#fff' size={24} />
-    Clientes
-    </Link>
-
-    <Link to='/profile'>
-    <FiSettings color='#fff' size={24} />
-    Perfil
-    </Link>
-
-    
-  </div>
-
-)
+  )
 }
