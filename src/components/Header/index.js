15e09b8c8b5c0ed24e8f593acca75b6256b75deb
Lucas Vinicius / INTELLEART <147888270+20026518-5@users.noreply.github.com>
2026-01-03 07:56:01 -0400

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,59 +1,43 @@
 import { useContext } from 'react';
-import avatarImg from '../../assets/avatar.png';   
-import { AuthContext } from '../../contexts/auth';
-import { FiHome, FiSettings, FiUser, FiLogOut, FiList } from 'react-icons/fi'; // Import único e completo
+import avatarImg from '../../assets/avatar.png';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/auth';
+import { FiHome, FiUser, FiSettings, FiUsers } from 'react-icons/fi'; // Adicionado FiUsers aqui
 import './header.css';
 
-export default function Header() {
-  const { user, logOut } = useContext(AuthContext);
+export default function Header(){
+  const { user } = useContext(AuthContext);
 
-  return (
-    <div className='sidebar'>
+  return(
+    <div className="sidebar">
       <div>
-        <img src={user.avatarUrl === null ? avatarImg : user.avatarUrl} alt='foto do usuario' />
+        <img src={user.avatarUrl === null ? avatarImg : user.avatarUrl} alt="Foto do usuario" />
       </div>
-      
-      <Link to='/dashboard'>
-        <FiHome color='#fff' size={24} />
+
+      <Link to="/dashboard">
+        <FiHome color="#FFF" size={24} />
         Chamados
       </Link>
 
-      <Link to='/customers'>
-        <FiUser color='#fff' size={24} />
-        Clientes
+      {/* Aba Servidores (Antiga Clientes) visível para todos ou apenas Adm conforme sua regra */}
+      <Link to="/servidores">
+        <FiUsers color="#FFF" size={24} />
+        Servidores
       </Link>
 
-      <Link to='/profile'>
-        <FiSettings color='#fff' size={24} />
+      <Link to="/profile">
+        <FiUser color="#FFF" size={24} />
         Perfil
       </Link>
 
-// Dentro do return do seu Header:
-<Link to="/servidores">
-  <FiUsers color="#FFF" size={24} />
-  Servidores
-</Link>
-
-{user.isadm && (
-  <Link to="/settings">
-    <FiSettings color="#FFF" size={24} />
-    Configurações
-  </Link>
-)}
-
-      {/* Aba de Setores visível apenas para Admin */}
+      {/* Rota de configurações para o ADM */}
       {user.isadm && (
-        <Link to='/settings'>
-          <FiList color='#fff' size={24} />
-          Setores
+        <Link to="/settings">
+          <FiSettings color="#FFF" size={24} />
+          Configurações
         </Link>
       )}
 
-      <button onClick={() => logOut()} className="logout-btn">
-        <FiLogOut color='#fff' size={24} />
-        Sair
-      </button>
     </div>
   )
 }
